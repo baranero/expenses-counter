990bde48a2b1f9b172096ead6b4dc4ffb863a24f
baranero <jakub.baran2@gmail.com>
2022-12-13 23:06:29 +0100

    lift the state up

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,14 +1,42 @@
 import React, { useState } from 'react'
 import './ExpenseForm.css'
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 
-    const titleChangeHandler = () => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+        }
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+        }
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+    
+    
+        const expenseData = {
+          title: enteredTitle,
+          amount: enteredAmount,
+          date: new Date(enteredDate),
+        };
+    
+        props.onSaveExpenseData(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     }
 
     return (
-        <form>
+    <form onSubmit={submitHandler}>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
